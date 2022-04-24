e85d806dbe85021c40984f77b1c650d27e980467
yogiyoazossi <chldnqja9688@naver.com>
2022-04-25 02:54:32 +0900

    2022.04.25

--- viewRoute53_BE/route53/src/main/java/aws/route53/repository/RecordSetsRepository.java
+++ viewRoute53_BE/route53/src/main/java/aws/route53/repository/RecordSetsRepository.java
@@ -1,12 +1,9 @@
 package aws.route53.repository;
 
 import aws.route53.entity.RecordSetsEntity;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,5 +21,6 @@ public interface RecordSetsRepository  extends CrudRepository<RecordSetsEntity, 
     void deleteByHostedZoneIdOrderByRecordSetsIdxDesc(String hostedZoneId);
 
     void deleteByRecordNameOrderByRecordSetsIdxDesc(String RecordName);
+
 }
 
